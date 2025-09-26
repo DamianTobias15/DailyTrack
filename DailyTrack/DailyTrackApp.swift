0f6271a4b9f153ac5d996a6b281f59a41e95b64c
Damian Tobias <erick_tls@hotmail.com>
2025-09-26 10:56:03 -0600

    Initial Commit

---
+++ DailyTrack/DailyTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyTrackApp.swift
+//  DailyTrack
+//
+//  Created by Erick Damian Tobias Valdez on 9/26/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DailyTrackTests/DailyTrackTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailyTrackTests.swift
+//  DailyTrackTests
+//
+//  Created by Erick Damian Tobias Valdez on 9/26/25.
+//
+
+import Testing
+@testable import DailyTrack
+
+struct DailyTrackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

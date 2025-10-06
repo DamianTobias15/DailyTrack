55122e042fc76dca8a45db1487a42215608f7b52
Damian Tobias <erick_tls@hotmail.com>
2025-10-06 12:35:07 -0600

    Fase 4: Persistencia local de tareas implementada con UserDefaults ✅

--- DailyTrack/TaskViewModel.swift
+++ DailyTrack/TaskViewModel.swift
@@ -3,13 +3,8 @@
 //  DailyTrack
 //
 //  Created by Erick Damian Tobias Valdez on 10/6/25.
-//
+//  Modify the add line code
 import Foundation
-
-import Foundation
-
-import Foundation
-
 import SwiftUI
 
 class TaskViewModel: ObservableObject {
@@ -19,15 +14,31 @@ class TaskViewModel: ObservableObject {
         }
     }
 
-    private let tasksKey = "dailytrack_tasks"
+    private let tasksKey = "tasks"
 
     init() {
         loadTasks()
     }
 
+    // Guardar tareas en UserDefaults
+    private func saveTasks() {
+        if let encoded = try? JSONEncoder().encode(tasks) {
+            UserDefaults.standard.set(encoded, forKey: tasksKey)
+        }
+    }
+
+    // Cargar tareas de UserDefaults
+    private func loadTasks() {
+        if let data = UserDefaults.standard.data(forKey: tasksKey),
+           let decoded = try? JSONDecoder().decode([Task].self, from: data) {
+            self.tasks = decoded
+        }
+    }
+
+    // Funciones de la Fase 3
     func addTask(title: String) {
-        let newTask = Task(title: title)
-        tasks.append(newTask)
+        let task = Task(id: UUID(), title: title, isCompleted: false)
+        tasks.append(task)
     }
 
     func toggleCompleted(_ task: Task) {
@@ -38,19 +49,5 @@ class TaskViewModel: ObservableObject {
 
     func deleteTask(at offsets: IndexSet) {
         tasks.remove(atOffsets: offsets)
-    }
-
-    // MARK: - Persistencia
-    private func saveTasks() {
-        if let encoded = try? JSONEncoder().encode(tasks) {
-            UserDefaults.standard.set(encoded, forKey: tasksKey)
-        }
-    }
-
-    private func loadTasks() {
-        if let data = UserDefaults.standard.data(forKey: tasksKey),
-           let decoded = try? JSONDecoder().decode([Task].self, from: data) {
-            tasks = decoded
-        }
     }
 }
